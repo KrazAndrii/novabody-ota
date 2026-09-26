36ed49d8fca7e1f50db4498867647f1412760c73
NovaBody OTA <ota@novabody.local>
2026-09-26 17:04:14 +0300

    OTA update

---
+++ assets/index-BUSD0IPE.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C5CKcJzk.js","assets/index-CpHsVPul.js","assets/index-Dzj9amZ8.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-CpHsVPul.js";const _=p("App",{web:()=>r(()=>import("./web-C5CKcJzk.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
